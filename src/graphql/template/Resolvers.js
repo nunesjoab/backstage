5ba9ca21ca62095218d6ea04093e12c5b15c60a6
Mariane Previde <mprevide@cpqd.com.br>
2019-02-14 15:00:41 -0200

    Fix comments

--- src/graphql/template/Resolvers.js
+++ src/graphql/template/Resolvers.js
@@ -91,7 +91,6 @@ const Resolvers = {
      */
     async templatesHasImageFirmware(root, { templatesId }, context) {
       setToken(context.token);
-      //setToken('Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnZW82dVYyamQ4TmtwQ1M4a2lZUkZmSVJqS1N6Rm5MaSIsImlhdCI6MTU1MDA1ODIyMiwiZXhwIjoxNTUwMDU4NjQyLCJwcm9maWxlIjoiYWRtaW4iLCJncm91cHMiOlsxXSwidXNlcmlkIjoxLCJqdGkiOiI5YTY2MGU1N2ExNTkwNDliY2RmMzMwYjlmYmQyMjgyNiIsInNlcnZpY2UiOiJhZG1pbiIsInVzZXJuYW1lIjoiYWRtaW4ifQ.WbIVkp72R2-iQOvnLKKdyyXVi8qJJGHutB1_ZFmcY0s');
       const map = [];
       const promises = [];
       templatesId.forEach((id) => {
